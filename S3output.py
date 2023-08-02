b3d309055e550b9a9fccdfdb98cb0fef35eb78e1
Admin <evaraa.engineer@gmail.com>
2023-08-02 10:28:17 +0530

    okay

---
+++ S3output.py
@@ -0,0 +1,25 @@
+from pyspark.sql import SparkSession
+
+# Create a SparkSession
+spark = SparkSession.builder \
+    .appName("Suresh") \
+    .getOrCreate()
+
+# Set the AWS access key and secret key
+spark._jsc.hadoopConfiguration().set("fs.s3a.access.key", "AKIAVDKIIDQNB3I5GAUG")
+spark._jsc.hadoopConfiguration().set("fs.s3a.secret.key", "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")
+
+# Specify the S3 path for the CSV file
+# s3_path = "s3a://evaraa/rootkey.csv"
+s3_path = "s3a://evaraa/customers-1000000.csv"
+s3_output_path = "s3a://evaraa/output.csv"
+
+
+# Read the CSV file into a DataFrame
+df1 = spark.read.csv(s3_path, header=True)
+
+# Show the DataFrame
+df1.show()
+
+# Save the DataFrame as a CSV file in the S3 bucket
+df1.write.csv(s3_output_path, header=True, mode="overwrite")
---
+++ While.py
@@ -0,0 +1,7 @@
+
+
+for suresh in range (1,10000000000000000):
+    print(suresh)
+
+
+
